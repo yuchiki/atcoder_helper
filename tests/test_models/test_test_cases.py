cdf837cc851a18bb9395038a4a82224d113ebcc7
yuchiki <14884013+yuchiki@users.noreply.github.com>
2022-08-10 14:18:00 +0000

    modelsの全関数についてtest or skipped testを実装

--- tests/test_models/test_test_cases.py
+++ tests/test_models/test_test_cases.py
@@ -2,7 +2,33 @@
 
 from typing import cast
 
+import pytest
+
 from atcoder_helper.models.test_case import AtcoderTestCase
+
+
+def test_atcoder_test_case_to_dict() -> None:
+    """AtcdoderTestCase.to_dict() works."""
+    test_cases = [
+        {
+            "name": "with expected",
+            "self": AtcoderTestCase("foo", "bar\n", "baz\n"),
+            "expected": {"name": "foo", "input": "bar\n", "expected": "baz\n"},
+        },
+        {
+            "name": "without expected",
+            "self": AtcoderTestCase("foo", "bar\n", None),
+            "expected": {
+                "name": "foo",
+                "input": "bar\n",
+            },
+        },
+    ]
+
+    for case in test_cases:
+        print(case["name"])
+        actual = cast(AtcoderTestCase, case["self"]).to_dict()
+        assert actual == case["expected"]
 
 
 def test_atcoder_test_case_from_dict() -> None:
@@ -24,3 +50,9 @@ def test_atcoder_test_case_from_dict() -> None:
         print(case["name"])
         actual = AtcoderTestCase.from_dict(cast(dict[str, str], case["input"]))
         assert actual == case["expected"]
+
+
+@pytest.mark.skip(reason="難しいので後でテストを書く")
+def test_atcoder_test_case_execute() -> None:
+    """Test for AtcoderTestCase.execute()"""
+    pass
