e3e3b6bdefbf65dd6168e3ce4c6f3ed9a522bdd0
yuchiki <14884013+yuchiki@users.noreply.github.com>
2022-08-14 14:45:10 +0000

    テストケースの雛型書いた

---
+++ tests/scripts/test_executor.py
@@ -0,0 +1,50 @@
+"""Tests for Executor."""
+
+
+import pytest
+
+
+class TestExecutor:
+    @pytest.mark.skip("TODO")
+    def test_auth_login_handler(self) -> None:
+        pass
+
+    @pytest.mark.skip("TODO")
+    def test_auth_logout_handler(self) -> None:
+        pass
+
+    @pytest.mark.skip("TODO")
+    def test_auth_status(self) -> None:
+        pass
+
+    @pytest.mark.skip("TODO")
+    def test_task_init_handler(self) -> None:
+        pass
+
+    @pytest.mark.skip("TODO")
+    def test_task_create_handler(self) -> None:
+        pass
+
+    @pytest.mark.skip("TODO")
+    def test_execute_test_handler(self) -> None:
+        pass
+
+    @pytest.mark.skip("TODO")
+    def test_fetch_task_handler(self) -> None:
+        pass
+
+    @pytest.mark.skip("TODO")
+    def test_config_init_handler(self) -> None:
+        pass
+
+    @pytest.mark.skip("TODO")
+    def test_config_languages_handler(self) -> None:
+        pass
+
+    @pytest.mark.skip("TODO")
+    def test_config_default_language_handler(self) -> None:
+        pass
+
+    @pytest.mark.skip("TODO")
+    def test_config_use_handler(self) -> None:
+        pass
