b4c12d11620ea89d9274de4ef13dd38d9ee1f439
yuchiki <14884013+yuchiki@users.noreply.github.com>
2022-08-21 00:15:36 +0900

    atcoder_repoを完全に消した (#7)

--- atcoder_helper/services/fetch_task.py
+++ atcoder_helper/services/fetch_task.py
@@ -1,10 +1,11 @@
 """testcasesを取得し、指定したtestcasesファイルに書き込むサービス."""
 from typing import Optional
 from typing import Protocol
+from typing import Tuple
 
 from atcoder_helper.repositories import errors as repository_error
-from atcoder_helper.repositories.atcoder_repo import AtCoderRepository
-from atcoder_helper.repositories.atcoder_repo import get_default_atcoder_repository
+from atcoder_helper.repositories.atcoder_test_case_repo import AtCoderTestCaseRepository
+from atcoder_helper.repositories.logged_in_session_repo import LoggedInSessionRepository
 from atcoder_helper.repositories.task_config_repo import TaskConfigRepository
 from atcoder_helper.repositories.task_config_repo import (
     get_default_task_config_repository,
@@ -47,20 +48,23 @@ def get_default_fetch_task_service() -> FetchTaskService:
 class FetchTaskServiceImpl:
     """atcoderサイトからテストケースを取得するサービス."""
 
-    _atcoder_repo: AtCoderRepository
     _task_config_repo: TaskConfigRepository
     _test_case_repo: TestCaseRepository
+    _session_repo: LoggedInSessionRepository
+    _atcoder_testcase_repo: AtCoderTestCaseRepository
 
     def __init__(
         self,
-        atcoder_repo: AtCoderRepository = get_default_atcoder_repository(),
         task_config_repo: TaskConfigRepository = get_default_task_config_repository(),
         test_case_repo: TestCaseRepository = get_default_test_case_repository(),
+        session_repo: LoggedInSessionRepository = LoggedInSessionRepository(),
+        atcoder_testcase_repo: AtCoderTestCaseRepository = AtCoderTestCaseRepository(),
     ) -> None:
         """__init__."""
-        self._atcoder_repo = atcoder_repo
         self._task_config_repo = task_config_repo
         self._test_case_repo = test_case_repo
+        self._session_repo = session_repo
+        self._atcoder_testcase_repo = atcoder_testcase_repo
 
     def fetch_task(
         self,
@@ -77,22 +81,17 @@ class FetchTaskServiceImpl:
             contest (str): コンテスト名。AtCoderのコンテストページのURLに現れる形式で渡す
             task (str): タスク名。AtCoderのコンテストページのURLに現れる形から、"コンテスト名_"の部分を除いたもの
         """
-        try:
-            task_config = self._task_config_repo.read()
-        except (repository_error.ReadError, repository_error.ParseError) as e:
-            raise ConfigAccessError("設定ファイルの読み込みに失敗しました") from e
-
-        contest = task_config.contest if contest is None else contest
-        task = task_config.task if task is None else task
-
-        if contest is None:
-            raise ConfigAccessError("contest is not set.")
-
-        if task is None:
-            raise ConfigAccessError("task is not set.")
+        (contest, task) = self._get_task_info(contest, task)
 
         try:
-            test_cases = self._atcoder_repo.fetch_test_cases(contest=contest, task=task)
+            session = self._session_repo.read()
+        except repository_error.ReadError as e:
+            raise ConfigAccessError("セッションの読み込みに失敗") from e
+
+        try:
+            test_cases = self._atcoder_testcase_repo.fetch_test_cases(
+                session=session, contest=contest, task=task
+            )
         except repository_error.ReadError as e:
             raise AtcoderAccessError("テストケースの取得に失敗しました") from e
 
@@ -100,3 +99,30 @@ class FetchTaskServiceImpl:
             self._test_case_repo.write(test_cases)
         except repository_error.WriteError as e:
             raise ConfigAccessError("テストケースの書き込みに失敗しました") from e
+
+    def _get_task_info(
+        self, given_contest: Optional[str], given_task: Optional[str]
+    ) -> Tuple[str, str]:
+        """contestとtaskを得る.
+
+        Raises:
+            ConfigAccessError: 設定に問題がある
+
+        Returns:
+            Tuple[str, str]: contest, task
+        """
+        try:
+            task_config = self._task_config_repo.read()
+        except (repository_error.ReadError, repository_error.ParseError) as e:
+            raise ConfigAccessError("設定ファイルの読み込みに失敗しました") from e
+
+        contest = task_config.contest if given_contest is None else given_contest
+        task = task_config.task if given_task is None else given_task
+
+        if contest is None:
+            raise ConfigAccessError("contest is not set.")
+
+        if task is None:
+            raise ConfigAccessError("task is not set.")
+
+        return (contest, task)
