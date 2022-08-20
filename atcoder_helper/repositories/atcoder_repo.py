15e821a66d415b9632cef3f102ff82234e924d96
yuchiki <14884013+yuchiki@users.noreply.github.com>
2022-08-20 09:30:07 +0000

    AtcoderTestCaseRepositoryを作り、fetch_test_casesを移譲

--- atcoder_helper/repositories/atcoder_repo.py
+++ atcoder_helper/repositories/atcoder_repo.py
@@ -10,8 +10,8 @@ import requests
 from bs4 import BeautifulSoup
 
 from atcoder_helper.models.test_case import AtcoderTestCase
+from atcoder_helper.repositories.atcoder_test_case_repo import AtCoderTestCaseRepository
 from atcoder_helper.repositories.errors import AlreadyLoggedIn
-from atcoder_helper.repositories.errors import ParseError
 from atcoder_helper.repositories.errors import ReadError
 from atcoder_helper.repositories.errors import WriteError
 from atcoder_helper.repositories.login_status_repo import LoginStatusRepo
@@ -202,47 +202,5 @@ class AtCoderRepositoryImpl:
         Returns:
             List[TestCase]: テストケーススイート
         """
-
-        def normalize_newline(text: str) -> str:
-            return "\n".join(text.splitlines())
-
-        try:
-            task_page = self._session.get(self.url_provider.task_url(contest, task))
-        except Exception as e:
-            raise ReadError(
-                f"cannot GET {self.url_provider.task_url(contest, task)}"
-            ) from e
-
-        try:
-            html = BeautifulSoup(task_page.text, "html.parser")
-
-            sections = (
-                html.find("div", id="task-statement")
-                .find("span", attrs={"class": "lang-ja"})
-                .find_all("section")
-            )
-
-            input_sections = {
-                section.find("h3").text.split()[1]: normalize_newline(
-                    section.find("pre").text
-                )
-                for section in sections
-                if "入力例" in section.find("h3").text
-            }
-
-            output_sections = {
-                section.find("h3").text.split()[1]: normalize_newline(
-                    section.find("pre").text
-                )
-                for section in sections
-                if "出力例" in section.find("h3").text
-            }
-        except Exception as e:
-            raise ParseError() from e
-
-        return [
-            AtcoderTestCase(
-                name=f"case-{name}", given=given, expected=output_sections[name]
-            )
-            for (name, given) in input_sections.items()
-        ]
+        atcoder_test_case_repo = AtCoderTestCaseRepository(self._session)
+        return atcoder_test_case_repo.fetch_test_cases(contest, task)
---
+++ atcoder_helper/repositories/atcoder_test_case_repo.py
@@ -0,0 +1,86 @@
+"""AtCoderからテストケースを取得するリポジトリ."""
+
+
+from typing import List
+
+import requests
+from bs4 import BeautifulSoup
+
+from atcoder_helper.models.test_case import AtcoderTestCase
+from atcoder_helper.repositories.errors import ParseError
+from atcoder_helper.repositories.errors import ReadError
+from atcoder_helper.repositories.utils import AtCoderURLProvider
+
+
+class AtCoderTestCaseRepository:
+    """AtCoderからテストケースを取得するリポジトリ."""
+
+    _session: requests.Session
+    _url_provider = AtCoderURLProvider
+
+    def __init__(self, session: requests.Session):
+        """__init__.
+
+        Args:
+            session (requests.Session): session
+        """
+        self._session = session
+
+    def fetch_test_cases(self, contest: str, task: str) -> List[AtcoderTestCase]:
+        """テストケーススイートを取得する.
+
+        Args:
+            contest (str): コンテスト名
+            task (str): タスク名
+
+        Raises:
+            ReadError: GETに失敗
+            ParseError: Parseに失敗
+
+        Returns:
+            List[TestCase]: テストケーススイート
+        """
+
+        def normalize_newline(text: str) -> str:
+            return "\n".join(text.splitlines())
+
+        try:
+            task_page = self._session.get(self._url_provider.task_url(contest, task))
+        except Exception as e:
+            raise ReadError(
+                f"cannot GET {self._url_provider.task_url(contest, task)}"
+            ) from e
+
+        try:
+            html = BeautifulSoup(task_page.text, "html.parser")
+
+            sections = (
+                html.find("div", id="task-statement")
+                .find("span", attrs={"class": "lang-ja"})
+                .find_all("section")
+            )
+
+            input_sections = {
+                section.find("h3").text.split()[1]: normalize_newline(
+                    section.find("pre").text
+                )
+                for section in sections
+                if "入力例" in section.find("h3").text
+            }
+
+            output_sections = {
+                section.find("h3").text.split()[1]: normalize_newline(
+                    section.find("pre").text
+                )
+                for section in sections
+                if "出力例" in section.find("h3").text
+            }
+        except Exception as e:
+            raise ParseError() from e
+
+        return [
+            AtcoderTestCase(
+                name=f"case-{name}", given=given, expected=output_sections[name]
+            )
+            for (name, given) in input_sections.items()
+        ]
