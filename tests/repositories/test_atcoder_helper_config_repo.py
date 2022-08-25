11557bf7de73f16ba6e3ea778f08f33edf6934fd
yuchiki <14884013+yuchiki@users.noreply.github.com>
2022-08-25 12:52:56 +0000

    readのテストを書いた

--- tests/repositories/test_atcoder_helper_config_repo.py
+++ tests/repositories/test_atcoder_helper_config_repo.py
@@ -13,12 +13,53 @@ from pytest import MonkeyPatch
 from atcoder_helper.models.atcoder_helper_config import AtCoderHelperConfig
 from atcoder_helper.models.atcoder_helper_config import LanguageConfig
 from atcoder_helper.repositories.atcoder_helper_config_repo import ConfigRepositoryImpl
+from atcoder_helper.repositories.errors import ParseError
+from atcoder_helper.repositories.errors import ReadError
 from atcoder_helper.repositories.errors import WriteError
 
 helper_config = AtCoderHelperConfig(
-    languages={"foo": LanguageConfig(name=" foo", build=[], run=[])},
+    languages={"foo": LanguageConfig(name="foo", build=[], run=[])},
     default_language="foo",
 )
+
+helper_config_dict = {
+    "languages": [{"name": "foo", "build": [], "run": []}],
+    "default_language": "foo",
+}
+
+
+test_config_repository_impl_read_parameters = {
+    "OK": [mock.MagicMock(return_value=helper_config_dict), None, helper_config],
+    "Error(読み込み失敗)": [mock.MagicMock(side_effect=OSError), ReadError, ANY],
+    "Error(パース失敗)": [mock.MagicMock(return_value={}), ParseError, ANY],
+}
+
+
+@pytest.mark.parametrize(
+    argnames=("safe_load_mock", "exception", "result"),
+    argvalues=test_config_repository_impl_read_parameters.values(),
+    ids=test_config_repository_impl_read_parameters.keys(),
+)
+def test_config_repository_impl_read(
+    safe_load_mock: mock.MagicMock,
+    exception: Type[Exception],
+    result: AtCoderHelperConfig,
+    monkeypatch: MonkeyPatch,
+) -> None:
+    """readのテスト."""
+    filename = "file_foo"
+
+    monkeypatch.setattr("builtins.open", mock.MagicMock())
+    monkeypatch.setattr(yaml, "safe_load", safe_load_mock)
+
+    sut = ConfigRepositoryImpl(filename=filename)
+
+    if exception:
+        with pytest.raises(exception):
+            sut.read()
+    else:
+        result = sut.read()
+        assert result == helper_config
 
 
 @pytest.mark.parametrize(
