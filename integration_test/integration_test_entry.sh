a387a6c973e44df8394fe0d8d0a9cc048f119f05
yuchiki <14884013+yuchiki@users.noreply.github.com>
2022-09-13 04:42:35 +0000

    環境変数をdocker containerに渡す

--- integration_test/integration_test_entry.sh
+++ integration_test/integration_test_entry.sh
@@ -3,4 +3,8 @@ set eux
 IMAGE_TAG=atcoder_helper_integration_environment
 
 docker build -f integration_test/Dockerfile -t $IMAGE_TAG .
-docker run $IMAGE_TAG integration_test/integration_test.sh
+docker run \
+    --env ATCODER_HELPER_NAME=${ATCODER_HELPER_NAME:-""} \
+    --env ATCODER_HELPER_PASSWORD=${ATCODER_HELPER_PASSWORD:-""} \
+    $IMAGE_TAG \
+    integration_test/integration_test.sh
